75d7eae07de82504fcfc6e61ed9ec24e83671579
ZeyadFayez <zeyadfayez5@gmail.com>
2025-03-25 23:30:21 +0200

    version 0.1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
+import { Box } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import CreatePage from "./Pages/CreatePage"
+import HomePage from "./Pages/HomePage"
+import NavBar from "./components/NavBar"
 function App() {
 
 
   return (
-    <>
-     
-      </>
-  )
+    
+      <Box minH={"100vh"}>
+        {<NavBar /> }
+        <Routes>
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Box>
+  );
 }
 
 export default App
