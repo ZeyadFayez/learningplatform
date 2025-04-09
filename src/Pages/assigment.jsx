6b1a1eac42e191482ee3978e052e1a4504088b89
ZeyadFayez <zeyadfayez5@gmail.com>
2025-04-09 02:44:44 +0200

    V3.4 After Vs code update

--- src/Pages/assigment.jsx
+++ src/Pages/assigment.jsx
@@ -2,27 +2,24 @@ import React from 'react';
 import Header from '../Component/header';
 import Footer from '../Component/footer';
 import Sidebar from '../Component/assignment-sidebar';
+import AssignmentMain from '../Component/assignment-main'; // Import AssignmentMain
 import { Box } from '@chakra-ui/react';
-import AssignmentMain from '../Component/assignment-main'; // Adjust the import path as needed
 
 const assigment = () => {
-    return(
+    return (
         <Box minH="100vh" bg="#DAD7CD">
-      
-        <Header />
-           
-        
-        <Box display="flex" flexDirection="row">
-        <Sidebar />
-         <AssignmentMain />
-        
+            <Header />
+            
+            <Box display="flex" flexDirection="row">
+                <Sidebar />
+                <Box flex="1" p={4}>
+                    <AssignmentMain /> {/* Render AssignmentMain */}
+                </Box>
+            </Box>
+            
+            <Footer />
         </Box>
-  
-       
-        <Footer />
-      </Box>
     );
-
-}
+};
 
 export default assigment;
